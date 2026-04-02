b5691ad3a13200929cc70978f75ea1c741444647
imnickio <imnickio3@gmail.com>
2026-04-02 14:02:46 -0500

    assignment

---
+++ assignment/database.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+URL_DATABASE = os.getenv("DATABASE_URL", "mysql+pymysql://root:password@db:3306/ItemDatabase")
+engine = create_engine(URL_DATABASE)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base=declarative_base()
